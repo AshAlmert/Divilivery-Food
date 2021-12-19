8415680fdfd799f214873751a0f2dfc5985eaa2a
AshAlmert <90694527+AshAlmert@users.noreply.github.com>
2021-12-19 18:57:24 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,64 +2,26 @@ const modal = document.querySelector('.modal');
 const buttonCart = document.querySelector('.button__cart');
 const buttonClosed = document.querySelector('.button-closed');
 const cancelButton = document.getElementById('cancelButton');
-const countSpan = document.querySelector('.count');
-const countBlocks = document.querySelectorAll('.count-block')
-const fullPrice =  document.querySelector('.fullprice');
+const missButton = document.getElementById('bodypart');
 
-let countMinButton;
-let countPlusButton;
-
-
-
-const toggleModal = function() {
-    modal.classList.toggle('open');
-}
-
-const removeOfModal = function(event) {
-    if (event.target.classList === modal.classList) {
-        modal.classList.remove('open')
-    }
-}
 
 buttonCart.addEventListener('click', toggleModal);
 buttonClosed.addEventListener('click', toggleModal);
 cancelButton.addEventListener('click', toggleModal);
-modal.addEventListener('click', removeOfModal);
+missButton.addEventListener('click', removeModal);
 
-for (let i = 0; i <countBlocks.Length; i++){
-    countMinButton = countBlock.querySelector('.count-min');
-    countPlusButton = countBlock.querySelector('.count-min');
-    let countSpan = countBlock.querySelector('.count');
-    const counterMinus = function () {
-        if(+countSpan.textContent === 0) {
-            countSpan.textContent = 0
-        } else {
-            count = +countSpan.textContent;
-            count--
-            countSpan.textContent = count;
-        }
-    };
-    const counterPlus = function () {
-        count = +countSpan.textContent;
-        count++
-        countSpan.textContent = count;
-    };
-
-    countMinButton.addEventListener('click', counterMinus);
-    countPlusButton.addEventListener('click', counterPlus);
+function toggleModal() {
+    modal.classList.toggle('open');
 }
 
-countMinButton.forEach(function(countMinButton) {
-    countMinButton.addEventListener('click', counterMinus);
-})
+if (missButton === 'bodypart') {
+    function removeModal() {
+        console.log(missButton);
+    }
+}
 
-countPlusButton.forEach(function(countPlusButton) {
-    countPlusButton.addEventListener('click', counterPlus);
-})
-
-
-console.log (typeof +countSpan.textContent)
 // console.log(modal);
 // console.log(buttonCart);
 // console.log(buttonClosed);
 //  console.log(cancelButton);\
+
